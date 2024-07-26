121f58a85e959a9a9452dbecc3d570da78a31285
Mohd Mudassir Ansari <mudassirimranansari@gmail.com>
2024-07-26 20:26:31 +0530

    updated: task view

--- src/Components/CompletedTask.js
+++ src/Components/CompletedTask.js
@@ -4,16 +4,18 @@ export default function CompletedTask(props) {
     const [completed, setcompleted] = useState(props.data.completed)
 
     return (
-        <li className="list-group-item d-flex justify-content-between align-items-start" style={{
+        <li className="list-group-item d-flex align-items-start justify-content-between"  style={{
             margin: '7px', backgroundColor: '#1a4062', boxShadow: '5px 5px 5px black'
         }}>
             <div className="ms-2 me-auto">
                 <div className="fw-bold" style={{ fontSize: '20px', color: 'white' }}>{props.data.task}</div>
             </div>
-            <div className="form-check form-switch"> <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={props.data.completed} />
+            <div className="d-flex justify-content-around">
+                <div className="form-check form-switch mx-3"> <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" checked={props.data.completed} />
+                </div>
+                <button type="button" class="btn btn-dark mx-3" onClick={() => props.renew(props.data.id)}>Renew Task</button>
+                <button type="button" class="btn btn-dark" onClick={() => props.delete(props.data.id)}>Delete Task</button>
             </div>
-            <button type="button" class="btn btn-dark " onClick={() => props.renew(props.data.id)}>Renew Task</button>
-            <button type="button" class="btn btn-dark" onClick={() => props.delete(props.data.id)}>Delete Task</button>
         </li>
     )
 }
